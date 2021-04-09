0352efe9fef50534d2e3bba6d5346d6030656788
Thomas Vandendijk <thomasvandendijk@gmail.com>
2021-04-09 22:37:24 +0200

    finished part 2

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/full', views.post_full, name='post_full'),
 ]
